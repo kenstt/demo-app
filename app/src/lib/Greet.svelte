194c692bee6d280e7906798803283d6c06fc3f5d
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-16 21:53:14 +0800

    在前端加 example api 的呼叫

--- app/src/lib/Greet.svelte
+++ app/src/lib/Greet.svelte
@@ -8,6 +8,14 @@
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsg = await invoke("greet", { name })
   }
+
+  let data = [];
+  const callMyApi = async () => {
+    const response = await fetch("https://dummy.restapiexample.com/api/v1/employees");
+    const body = await response.json();
+    data = body.data;
+    console.log(data);    // 我們直接看資料的長相，再回來調整程式碼
+  }
 </script>
 
 <div>
@@ -16,4 +24,10 @@
     <button type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
+  <button on:click={callMyApi}>Call my API</button>
+  <ul>
+    {#each data as item}
+      <li>{item.employee_name}</li>
+    {/each}
+  </ul>
 </div>
