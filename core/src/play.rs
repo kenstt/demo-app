d7f0f8eeb5e81e79dfbe95fb2f5f6657c7b6fc2d
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-17 22:19:20 +0800

    加井字遊戲的play方法初稿

--- core/src/play.rs
+++ core/src/play.rs
@@ -1,8 +1,13 @@
 use core::tic_tac_toe::{Game, Cell};
 
 fn main() {
-    let game = Game {
-        cells: [Cell::Empty; 9]
-    };
-    println!("{:?}", game);
+    let mut game = Game::default();
+    println!("{}", game);
+    loop {                               // 開始迴圈
+        println!("請輸入數字 1 and 9");    // 提示用戶輸入
+        let num = 1;               // 取得用戶輸入
+        game.play(num);                  // 處理遊戲邏輯
+        if game.is_over { break; }       // 若遊戲結束，離開迴圈
+    }
+    println!("{}", game);                // 印出結果
 }
--- core/src/tic_tac_toe.rs
+++ core/src/tic_tac_toe.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 pub enum Player {
     A,
     B,
@@ -13,4 +15,53 @@ pub enum Cell {
 #[derive(Debug)]
 pub struct Game {
     pub cells: [Cell; 9],
+    pub is_over: bool,
+}
+
+impl Game {
+    // 替Game加上方法
+    pub fn play(&mut self, num: usize) {   // 這裡的方法參數為自己與外部輸入值
+        self.cells[num] = Cell::O;
+        self.is_over = true;
+    }
+}
+
+impl Default for Game {
+    // 替Game實作預設(無參數建構式)
+    fn default() -> Self {     // 這裡的大寫Self指的是Self的Type，就是指Game
+        Self {                 // 所以這邊的兩個Self換成Game也是一樣的結果
+            cells: [Cell::Empty; 9],
+            is_over: false,
+        }
+    }
+}
+
+impl Display for Game {
+    // 替 Game 實作 Display 這個 trait
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "
+        {} | {} | {}
+        ---------
+        {} | {} | {}
+        ---------
+        {} | {} | {}
+        ",
+            self.cells[0], self.cells[1], self.cells[2],
+            self.cells[3], self.cells[4], self.cells[5],
+            self.cells[6], self.cells[7], self.cells[8],
+        )
+    }
+}
+
+impl Display for Cell {
+    // 替 Cell 實現 Display 這個 trait
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Cell::Empty => write!(f, " "),
+            Cell::O => write!(f, "O"),
+            Cell::X => write!(f, "X"),
+        }
+    }
 }
