b32e8c6b671a06a462453831a3d8731b1c78c4e2
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-18 23:55:09 +0800

    重構 O/X 符號並清理

--- core/src/tic_tac_toe.rs
+++ core/src/tic_tac_toe.rs
@@ -1,29 +1,23 @@
 use std::fmt::{Display, Formatter};
 
-pub enum Player {
-    A,
-    B,
-}
-
 #[derive(Copy, Clone, Debug)]
-pub enum Cell {
-    Empty,
+pub enum Symbol {
     O,
     X,
 }
 
 #[derive(Debug)]
 pub struct Game {
-    pub cells: [Cell; 9],
+    pub cells: [Option<Symbol>; 9],
     pub is_over: bool,
-    pub winner: Option<Cell>,
+    pub winner: Option<Symbol>,
 }
 
 impl Game {
     // 替Game加上方法
     pub fn play(&mut self, num: usize) {   // 這裡的方法參數為自己與外部輸入值
         let index = num - 1;
-        self.cells[index] = Cell::O;
+        self.cells[index] = Some(Symbol::O);
     }
 }
 
@@ -31,7 +25,7 @@ impl Default for Game {
     // 替Game實作預設(無參數建構式)
     fn default() -> Self {     // 這裡的大寫Self指的是Self的Type，就是指Game
         Self {                 // 所以這邊的兩個Self換成Game也是一樣的結果
-            cells: [Cell::Empty; 9],
+            cells: [None; 9],
             is_over: false,
             winner: None,
         }
@@ -50,20 +44,26 @@ impl Display for Game {
         ---------
         {} | {} | {}
         ",
-            self.cells[0], self.cells[1], self.cells[2],
-            self.cells[3], self.cells[4], self.cells[5],
-            self.cells[6], self.cells[7], self.cells[8],
+            show(self.cells[0]), show(self.cells[1]), show(self.cells[2]),
+            show(self.cells[3]), show(self.cells[4]), show(self.cells[5]),
+            show(self.cells[6]), show(self.cells[7]), show(self.cells[8]),
         )
     }
 }
 
-impl Display for Cell {
+fn show(cell: Option<Symbol>) -> String {
+    match cell {
+        Some(symbol) => format!("{}", symbol),
+        None => " ".to_string(),
+    }
+}
+
+impl Display for Symbol {
     // 替 Cell 實現 Display 這個 trait
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Cell::Empty => write!(f, " "),
-            Cell::O => write!(f, "O"),
-            Cell::X => write!(f, "X"),
+            Symbol::O => write!(f, "O"),
+            Symbol::X => write!(f, "X"),
         }
     }
 }
