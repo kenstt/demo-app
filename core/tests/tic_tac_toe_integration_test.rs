739cf7b02aa68a357b45527d004ab231dd1a2ffb
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-19 22:03:47 +0800

    加整合測試

---
+++ core/tests/tic_tac_toe_integration_test.rs
@@ -0,0 +1,39 @@
+use core::tic_tac_toe::{Game, Symbol};
+
+#[test]
+fn test_game1_for_x_win() {          // 測試X贏的情境
+    let mut game = Game::default();
+    game.play(3).unwrap();           // O
+    game.play(1).unwrap();           // X
+    game.play(2).unwrap();           // O
+    let err = game.play(1);          // 下到非空的格子
+    assert_eq!(err.is_err(), true);  // 應該會回傳錯誤
+
+    game.play(4).unwrap();           // X
+    game.play(5).unwrap();           // O
+    assert_eq!(game.is_over, false); // 確定分出勝負前的狀態
+    assert_eq!(game.winner, None);   // 棋局尚未結束，且尚無贏家
+    game.play(7).unwrap();           // X -> 連成一線 1, 4, 7
+    assert_eq!(game.is_over, true);  // 棋局狀態為結束
+    assert_eq!(game.winner, Some(Symbol::X)); // 棋局贏家為 O
+
+    let err = game.play(6);
+    assert_eq!(err.is_err(), true);
+}
+
+#[test]
+fn test_game2_for_draw() {           // 模擬平手情境
+    let mut game = Game::default();
+    game.play(1).unwrap();           // O
+    game.play(4).unwrap();           // X
+    game.play(2).unwrap();           // O
+    game.play(5).unwrap();           // X
+    game.play(6).unwrap();           // O
+    game.play(3).unwrap();           // X
+    game.play(7).unwrap();           // O
+    game.play(8).unwrap();           // X
+    assert_eq!(game.is_over, false);
+    game.play(9).unwrap();           // O
+    assert_eq!(game.is_over, true);  // 棋局已結束
+    assert_eq!(game.winner, None);   // 但無玩家勝出
+}
