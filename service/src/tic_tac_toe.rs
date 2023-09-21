829e298e8b8bcbcd6b3a3cb8e7cd9e9d1c9369bd
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-22 00:10:54 +0800

    完成 Read 及 測試

--- service/src/tic_tac_toe.rs
+++ service/src/tic_tac_toe.rs
@@ -1,6 +1,6 @@
 use core::tic_tac_toe::Game;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     GameRules(String),
     GameOver,
@@ -45,7 +45,15 @@ impl TicTacToeService for InMemoryTicTacToeService {
     }
 
     fn get(&self, id: usize) -> Result<Game, Error> {
-        todo!()
+        let games = self.games.lock().unwrap();
+        games.get(&id).cloned().ok_or(Error::NotFound)
+        // 或
+        // games.get(&id).map(|game| game.clone()).ok_or(Error::NotFound)
+        // 或
+        // match games.get(&id) {
+        //     Some(game) => Ok(game.clone()),
+        //     None => Err(Error::NotFound),
+        // }
     }
 
     fn play(&self, id: usize, num: usize) -> Result<Game, Error> {
@@ -71,4 +79,23 @@ mod tests {
         // let is_empty = game.cells.iter().all(|x| *x == None); // 或是這樣寫
         assert_eq!(is_empty, true);
     }
+
+    #[test]
+    fn test_get() {                                    // 測試 Happy Path
+        let service = InMemoryTicTacToeService::new();
+        let _ = service.new();                         // 新局的變數我們不需要，直接 _ 丟棄
+        let game = service.get(1).unwrap();            // 透過 get 取得 game
+        assert_eq!(game.is_over, false);               // 簡單驗一下內容
+        let is_empty = game.cells.iter().all(|x| *x == None);
+        assert_eq!(is_empty, true);
+    }
+
+    #[test]
+    fn test_get_none() {                               // 測試 None 會回傳Error
+        let service = InMemoryTicTacToeService::new();
+        let game = service.get(10);                    // Result<Game, Error>
+        assert_eq!(game.is_err(), true);               // 驗證回傳Error
+        assert_eq!(game.err(), Some(Error::NotFound)); // 驗證Error類別
+    }
 }
+
