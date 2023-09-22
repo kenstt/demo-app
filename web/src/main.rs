af2fe0dec45ec18467f906d7773f7f0b3b519661
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-23 00:18:56 +0800

    加 logger 機制：終端及檔案
    (cherry picked from commit 30f791035c4aacadf5817fc11dd574b39e5faedc)

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,9 +1,40 @@
 use warp::Filter;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+    let filter = std::env::var("RUST_LOG")
+        .unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+
+    let file_appender = tracing_appender::rolling::daily("./logs", "log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::registry()
+        .with(tracing_filter())
+        .with(tracing_subscriber::fmt::layer())  // 輸出至終端
+        .with(tracing_subscriber::fmt::layer()
+            .with_writer(non_blocking))          // 輸出至檔案
+        .init();
+
+    let hello = warp::path("hello")
+        .and(warp::get())
+        .map(|| {
+            tracing::info!("saying hello...");
+            "Hello, World!"
+        })
+        .with(warp::trace::named("hello"));
+
+    let routes = hello
+        .with(warp::trace::request());
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+pub fn tracing_filter() -> EnvFilter {
+    EnvFilter::from_default_env()
+        .add_directive("warp=debug".parse().unwrap())
+        .add_directive("web=debug".parse().unwrap())
+        .add_directive("tracing=debug".parse().unwrap())
 }
