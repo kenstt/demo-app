6b1a65f76746e5ea20f90a92082a700e82beb470
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-23 00:55:28 +0800

    logger層級改設為讀取環境變數 .env
    (cherry picked from commit f5c0a7caf0136079ed33b68a4492956055d777b1)

--- web/src/logger.rs
+++ web/src/logger.rs
@@ -1,11 +1,16 @@
 use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+use crate::config::ENV_LOG_LEVEL;
 
 pub fn tracing_filter() -> EnvFilter {
+    let level = std::env::var(ENV_LOG_LEVEL).unwrap_or_else(|_| "info".to_string());
+    let dir1 = format!("warp={}", level);
+    let dir2 = format!("web={}", level);
+    let dir3 = format!("tracing={}", level);
     EnvFilter::from_default_env()
-        .add_directive("warp=debug".parse().unwrap())
-        .add_directive("web=debug".parse().unwrap())
-        .add_directive("tracing=debug".parse().unwrap())
+        .add_directive(dir1.parse().unwrap())
+        .add_directive(dir2.parse().unwrap())
+        .add_directive(dir3.parse().unwrap())
 }
 
 pub fn register_tracing(non_blocking: NonBlocking) {
