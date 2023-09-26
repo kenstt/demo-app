8e1dd2081efaa8879a25e2bbf89af8e28e6ed0e2
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-26 21:21:33 +0800

    在tauri中加入http client

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -7,9 +7,43 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use std::collections::HashMap;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Http(#[from] reqwest::Error),
+}
+
+// we must manually implement serde::Serialize
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
+#[tauri::command]
+async fn http_test() -> Result<(), Error> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+
+    Ok(())
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            http_test,    // 這裡要把我們設的command加進來，前端才能呼叫
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- app/src/lib/Greet.svelte
+++ app/src/lib/Greet.svelte
@@ -8,7 +8,17 @@
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsg = await invoke('greet', { name });
   }
+
+  const test_api = async () => {
+    const res = await invoke('http_test');
+    console.log(res);
+  }
 </script>
+
+<button
+  class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+  on:click={test_api}> 測試tauri reqwest
+</button>
 
 <div>
   <form class="row" on:submit|preventDefault={greet}>
