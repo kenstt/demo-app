926bb50372fa7d81913a59f5319f1a297a097dfb
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-26 23:17:09 +0800

    調整前端 api client 同時支援 tauri及 web

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -5,6 +5,12 @@ export interface Api {
   ticTacToe: TicTacToeApi;
 }
 
-export const api: Api = {
+const httpApi: Api = {    // 把之前 api rename 成 httpApi 區別 tauri
+  ticTacToe: ticTacToeApi,
+};
+
+const tauriApi: Api = {    // 這部分沒有動
   ticTacToe: ticTacToeApiTauri,
 };
+
+export const api: Api = window.__TAURI_IPC__ ? tauriApi : httpApi;
