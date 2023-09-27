cd596ea72776705e20b69052ec7d4538d520ca40
kenstt <144754075+kenstt@users.noreply.github.com>
2023-09-27 23:43:48 +0800

    example-lifetime 加 fn 生命週期 (1)

--- examples/lifetime/src/main.rs
+++ examples/lifetime/src/main.rs
@@ -23,7 +23,7 @@ fn move_student(s: LifeStudent) {
     s.no;
 }
 
-fn main() {
+fn main_struct() {
     let student = LifeStudent { no: &1, name: &"John".to_string() };
     move_string(student.name.to_string());
     println!("student no, {}!", student.no);
@@ -31,4 +31,23 @@ fn main() {
     move_student(student);                       // 這裡move student
     // println!("student no, {}!", student.no);     // 移除會報錯
     // println!("student name, {}!", student.name); // 移除會報錯
+}
+
+fn longer_string(s1: String, s2: String) -> String { // 取長的字串
+    if s1.len() > s2.len() {
+        s1        // 把 s1 所有權移交給fn呼叫者
+    } else {
+        s2        // 把 s2 所有權移交給fn呼叫者
+    }             // s1, s2 都被 drop
+}
+
+fn main() {
+    let a = String::from("Rust");
+    let b = String::from("Svelte");
+    // let longer = longer_string(a, b);    // a 和 b 都被move了
+    let longer = longer_string(a.clone(), b.clone());
+
+    println!("a: len: {:2}, {:?}", a.len(), a); // :2 是佔用2格位置
+    println!("b: len: {:2}, {:?}", b.len(), b); // :? 是Debug，字串會加"
+    println!("   longer is >> {:?} <<", longer);
 }
