7ffa1d2f5392afe9026b8654658271da6449ff16
kenstt <144754075+kenstt@users.noreply.github.com>
2023-10-02 00:59:06 +0800

    加入溢位範例2,3

--- examples/overflow/src/main.rs
+++ examples/overflow/src/main.rs
@@ -1,5 +1,18 @@
-fn main() {
+fn main1() {
     let num: u8 = 200;               // u8 上限是 255
     let num2: u8 = 100;              // u8 上限是 255
-    println!("num: {}", num + num2); // u8 + u8 還是 u8，上限255
+    // println!("num: {}", num + num2); // u8 + u8 還是 u8，上限255
+}
+
+fn main2() {
+    let num: u8 = 5;
+    let input = "255";
+    let num2: u8 = input.parse().unwrap();
+    println!("num: {}", num + num2);
+}
+
+fn main() {
+    let num: u16 = 300;
+    let result = u8::try_from(num);
+    println!("num: {:?}", result);
 }
