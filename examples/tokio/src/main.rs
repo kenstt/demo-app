ccbe22c12436c15bf26b7e22bc0e9c61c04e1fd5
kenstt <144754075+kenstt@users.noreply.github.com>
2023-10-05 22:47:59 +0800

    tokio 範例專案加分身

--- examples/tokio/src/main.rs
+++ examples/tokio/src/main.rs
@@ -1,13 +1,31 @@
+use rand::random;
+use chrono::Utc;
 
 #[tokio::main]
 async fn main() {
-    println!("======程式開始======");        // 標注讓我們知道發生什麼事
-    tokio::spawn(async {                    // 分身詠唱之術
+    println!("======程式開始======");
+    tokio::spawn(async {            // === 分身 part ===
         for i in 1..6 {
-            println!("分身-攻{}", i);        // 模擬分身做一些事情
+            let rand = random::<u64>() % 1000;    // 產生隨機數 0~1000 作為等待豪秒
+            let delay = std::time::Duration::from_millis(rand);
+            tokio::time::sleep(delay).await;
+            let padding = " ".repeat((1 * 9) as usize);   // 插入空白讓版面整齊
+            println!("{:12}: {} \x1b[94m分身-攻{}\x1b[0m",
+                     Utc::now().format("%S%.f"),
+                     padding,
+                     i);
         }
     });
+    for i in 1..6 {                // === 主體 part ===
+        let rand = random::<u64>() % 1000;
+        let delay = std::time::Duration::from_millis(rand);
+        tokio::time::sleep(delay).await;
+        println!("{:12}: \x1b[93m主體-攻{}\x1b[0m",
+                 Utc::now().format("%S%.f"),
+                 i);
+    }
+    // tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
     println!("======程式結束======");
     let mut line = String::new();
-    let _ = std::io::stdin().read_line(&mut line).unwrap(); // 等待終端輸入的資料
+    let _ = std::io::stdin().read_line(&mut line).unwrap();
 }
