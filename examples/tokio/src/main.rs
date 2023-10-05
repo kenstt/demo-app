499434e1ed00b4eea86a0592f2253e9c64b8ce22
kenstt <144754075+kenstt@users.noreply.github.com>
2023-10-06 00:02:02 +0800

    開啟分身跑mpsc

--- examples/tokio/src/main.rs
+++ examples/tokio/src/main.rs
@@ -2,7 +2,7 @@ use rand::random;
 use chrono::Utc;
 
 #[tokio::main]
-async fn main() {
+async fn main_5分身() {
     println!("======程式開始======");
     for p in 1..6 {                    // 再多包一層 p for person
         tokio::spawn(async move {      // 這裡必需多一個 move
@@ -30,4 +30,40 @@ async fn main() {
     println!("======程式結束======");
     let mut line = String::new();
     let _ = std::io::stdin().read_line(&mut line).unwrap();
+}
+
+#[tokio::main]
+async fn main() {  // mpsc
+    println!("======程式開始======");
+    let (tx, rx) = std::sync::mpsc::channel();    // 產生事件通道
+    let tx1 = tx.clone();               // tx是發送，透過clone產生多個發送者
+    tokio::spawn(async move {
+        for i in 1..6 {
+            let rand = random::<u64>() % 1000;
+            let delay = std::time::Duration::from_millis(rand);
+            tokio::time::sleep(delay).await;
+            let padding = " ".repeat((1 * 14) as usize);
+            println!("{:12}: {} \x1b[93m客戶1 tx：{}\x1b[0m",
+                     Utc::now().format("%S%.f"), padding, rand);
+            tx1.send((i, rand)).unwrap();        // tx1 被move進來，後面不能再使用
+        }
+    });
+    let tx2 = tx.clone();                        // 產生另一個發送者tx
+    tokio::spawn(async move {
+        for i in 1..6 {
+            let rand = random::<u64>() % 1000;
+            let delay = std::time::Duration::from_millis(rand);
+            tokio::time::sleep(delay).await;
+            let padding = " ".repeat((2 * 14) as usize);
+            println!("{:12}: {} \x1b[91m客戶2 tx：{}\x1b[0m",
+                     Utc::now().format("%S%.f"), padding, rand);
+            tx2.send((i, rand)).unwrap();        // 傳遞資料進channel queue
+        }
+    });
+    loop {                                       // 一直在監聽
+        let msg = rx.recv().unwrap();            // 如果接收到訊息再往下處理
+        let (i, rand) = msg;
+        println!("{:12}: \x1b[95m主機rx：{}\x1b[0m",
+                 Utc::now().format("%S%.f"), rand);
+    }                                            // forever loop
 }
