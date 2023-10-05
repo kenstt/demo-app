1abe544382d30890da17009fd579a200f5c1bd81
kenstt <144754075+kenstt@users.noreply.github.com>
2023-10-05 23:03:08 +0800

    開啟5分身

--- examples/tokio/src/main.rs
+++ examples/tokio/src/main.rs
@@ -4,18 +4,20 @@ use chrono::Utc;
 #[tokio::main]
 async fn main() {
     println!("======程式開始======");
-    tokio::spawn(async {            // === 分身 part ===
-        for i in 1..6 {
-            let rand = random::<u64>() % 1000;    // 產生隨機數 0~1000 作為等待豪秒
-            let delay = std::time::Duration::from_millis(rand);
-            tokio::time::sleep(delay).await;
-            let padding = " ".repeat((1 * 9) as usize);   // 插入空白讓版面整齊
-            println!("{:12}: {} \x1b[94m分身-攻{}\x1b[0m",
-                     Utc::now().format("%S%.f"),
-                     padding,
-                     i);
-        }
-    });
+    for p in 1..6 {                    // 再多包一層 p for person
+        tokio::spawn(async move {      // 這裡必需多一個 move
+            for i in 1..6 {
+                let rand = random::<u64>() % 1000;
+                let delay = std::time::Duration::from_millis(rand);
+                tokio::time::sleep(delay).await;
+                let padding = " ".repeat((p * 9) as usize);    // 把外面的 p move 進來
+                println!(
+                    "{:12}: {} \x1b[9{}m分{}-攻{}\x1b[0m",
+                    Utc::now().format("%S%.f"),
+                    padding, p, p, i);
+            }
+        });
+    }
     for i in 1..6 {                // === 主體 part ===
         let rand = random::<u64>() % 1000;
         let delay = std::time::Duration::from_millis(rand);
